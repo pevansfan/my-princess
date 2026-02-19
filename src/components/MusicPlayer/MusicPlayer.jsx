1903509d00557167ee92d9d69ea3b66e528b4bcb
Evans PARFAIT <fani.prft@gmail.com>
2026-02-19 23:23:08 +0100

    Gestion du loading pour l'audio

--- src/components/MusicPlayer/MusicPlayer.jsx
+++ src/components/MusicPlayer/MusicPlayer.jsx
@@ -7,12 +7,15 @@ import "./MusicPlayer.css";
 export default function MusicPlayer() {
     const [index, setIndex] = useState(0);
     const [isPlaying, setIsPlaying] = useState(false);
+    const [loading, setLoading] = useState(true);
     const [progress, setProgress] = useState(0);
     // const [volume, setVolume] = useState(0.7);
 
     const audioRef = useRef(new Audio(`${process.env.PUBLIC_URL}/${playlist[index].src}`));
 
     useEffect(() => {
+        setLoading(true);
+
         audioRef.current.pause();
         audioRef.current = new Audio(`${process.env.PUBLIC_URL}/${playlist[index].src}`);
         // audioRef.current.volume = volume;
@@ -25,7 +28,16 @@ export default function MusicPlayer() {
 
         audioRef.current.onended = next;
 
-        if (isPlaying) audioRef.current.play();
+        const handleCanPlay = () => {
+            setLoading(false);
+            if (isPlaying) audioRef.current.play();
+        };
+
+        audioRef.current.addEventListener("canplaythrough", handleCanPlay);
+
+        return () => {
+            audioRef.current.removeEventListener("canplaythrough", handleCanPlay);
+        };
     }, [index, isPlaying]);
 
     const playPause = () => {
@@ -59,7 +71,7 @@ export default function MusicPlayer() {
                 <img
                     key={index}
                     src={`${process.env.PUBLIC_URL}/${playlist[index].cover}`}
-                    className={`cover ${isPlaying ? "playing" : ""}`}
+                    className={`cover ${isPlaying && !loading ? "playing" : ""}`}
                     alt="cover"
                 />
 
@@ -86,7 +98,7 @@ export default function MusicPlayer() {
                     >
                         <LuArrowLeftToLine />
                     </button>
-                    <button className="play" onClick={playPause}>
+                    <button className="play" onClick={playPause} disabled={loading}>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="size-6">
                             <path d="M15 6.75a.75.75 0 0 0-.75.75V18a.75.75 0 0 0 .75.75h.75a.75.75 0 0 0 .75-.75V7.5a.75.75 0 0 0-.75-.75H15ZM20.25 6.75a.75.75 0 0 0-.75.75V18c0 .414.336.75.75.75H21a.75.75 0 0 0 .75-.75V7.5a.75.75 0 0 0-.75-.75h-.75ZM5.055 7.06C3.805 6.347 2.25 7.25 2.25 8.69v8.122c0 1.44 1.555 2.343 2.805 1.628l7.108-4.061c1.26-.72 1.26-2.536 0-3.256L5.055 7.061Z" />
                         </svg>
